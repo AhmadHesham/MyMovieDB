eaba86ae7e5487275683c1c5a541d10bdc7c25f4
AhmadHesham <ahmedhsaid@gmail.com>
2021-06-12 02:48:18 +0200

    Fix wrong import

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Carousel from 'react-material-ui-carousel';
 import axios from 'axios'
-import Poster from '../cards/Preview';
+import Preview from '../cards/Preview';
+import Poster from '../cards/Poster'
 
 const useStyles = makeStyles({
     root: {
@@ -11,7 +12,8 @@ const useStyles = makeStyles({
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        flexDirection: 'column'
+        flexDirection: 'column',
+        overflowY: 'scroll'
     },
     carouselContainer: {
         height: '40%',
@@ -29,12 +31,19 @@ export default function Home() {
     const [imageHeight, setImageHeight] = React.useState();
     const apiKey = '6355f15310ac756b161ac38dda6299f7';
 
+    const carouselObserver = new ResizeObserver((entries) => {
+        const rect = entries[0].contentRect;
+        setImageHeight(rect.height);
+        console.log(rect.height)
+    });
+
     React.useEffect(() => {
         const loadMovies = async () => {
             axios({
                 method: 'GET',
                 url: `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
             }).then(res => {
+                console.log(res.data.results)
                 setMovies(res.data.results);
             }).catch(err => {
                 console.log(err);
@@ -42,26 +51,20 @@ export default function Home() {
         }
         loadMovies();
         carouselObserver.observe(document.querySelector("#carouselContainer"));
-    }, [])
-
-    const carouselObserver = new ResizeObserver((entries) => {
-        const rect = entries[0].contentRect;
-        setImageHeight(rect.height);
-        console.log(rect.height)
-    });
+    }, []);
 
     return (
         <div className={classes.root}>
-            <div style={{width: '100%', height: '92vw', marginTop: '8vw'}}>
+            <div style={{ width: '100%', height: '92vw', marginTop: '8vw' }}>
                 <div id="carouselContainer" className={classes.carouselContainer}>
                     <Carousel>
                         {
-                            movies.map((movie, key) => <Poster key={key} movie={movie} height={imageHeight} />)
+                            movies.map((movie, key) => <Preview key={key} movie={movie} height={imageHeight} />)
                         }
                     </Carousel>
                 </div>
-                {/* <div style={{ height: '50%', width: '100%' }}> */}
-                {/* </div> */}
+                <div style={{ height: '50%', width: '100%' }}>
+                </div>
             </div>
         </div>
     )
